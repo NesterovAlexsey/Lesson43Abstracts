64d6529b3b988a4f1107852177e87c6247386a85
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-19 07:09:53 +0100

    add getter and setter to the House

---
+++ src/House.java
@@ -0,0 +1,20 @@
+//    Создайте абстрактный класс "Дом".
+public abstract class House {
+
+  String address;
+
+  //В качестве аргумента конструктора принимайте адрес.
+  public House(String address) {
+    this.address = address;
+  }
+
+  //    Напишите для адреса геттер.
+  public String getAddress() {
+    return this.address;
+  }
+
+  //    Напишите для адреса сеттер
+  public void setAddress(String address) {
+    this.address = address;
+  }
+}
