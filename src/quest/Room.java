07abb6f43625b606ac54cdd8faa2729dee86fa74
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-20 16:58:20 +0100

    Add some logic with 2d List

--- src/quest/Room.java
+++ src/quest/Room.java
@@ -1,8 +1,16 @@
 package quest;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
 
   private final String title;
+
+  private static int i = 0;
+
+  private static int j = 0;
 
   public Room(String title) {
     this.title = title;
@@ -10,5 +18,26 @@ public class Room {
 
   public String getTitle() {
     return title;
+  }
+
+  public void roomCoordinate(int x, int y) {
+
+    ArrayList<String> corridorWc = new ArrayList<>();
+    corridorWc.add("Вы находитесь в коридоре");
+    corridorWc.add("Вы находитесь в туалете");
+
+    ArrayList<String> hallKitchen = new ArrayList<>();
+    hallKitchen.add("Вы находитесь в зале");
+    hallKitchen.add("Вы находитесь в кухне");
+
+    ArrayList<ArrayList<String>> rooms = new ArrayList<>();
+    rooms.add(corridorWc);
+    rooms.add(hallKitchen);
+
+    i = i + x;
+    j = j + y;
+
+    System.out.println(rooms.get(i).get(j));
+
   }
 }
